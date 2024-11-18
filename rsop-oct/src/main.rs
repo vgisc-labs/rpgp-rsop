4a238d7dd76a7e49c4a9f8e34ae5063e1cd56cce
Heiko Schaefer <heiko@schaefer.name>
2024-11-18 04:31:25 +0100

    xx rsop-oct: sop 0.8

--- rsop-oct/src/main.rs
+++ rsop-oct/src/main.rs
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: CC0-1.0
 
 fn main() {
+    #[cfg(feature = "cliv")]
+    let variant = sop::cli::Variant::Verification;
+
+    #[cfg(feature = "cli")]
+    let variant = sop::cli::Variant::Full;
+
     env_logger::init();
-    sop::cli::main(&rpgpie_sop_oct::RPGSOPOCT::default());
+    sop::cli::main(&mut rpgpie_sop_oct::RPGSOPOCT::default(), variant);
 }
